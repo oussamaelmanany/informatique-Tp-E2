cfd864af3b1fe07436f6fca89350f40335b5c883
OUSSAMA EL MANANY <Oussama.elmanany@reseau.eseo.fr>
2026-09-10 08:41:22 +0200

    Fin exercice 03

--- TP01.py
+++ TP01.py
@@ -1,6 +1,31 @@
 #Exercice 01 :
-def IMC(Poids, taille):
+
+def IMC():
     Poids= int(input("Entrez votre poids en kg : "))
     taille= float(input("Entrez votre taille en m : "))
     IMC= Poids/(taille*taille)
-print("Votre IMC est de : ", IMC)
+    print("Votre IMC est de : ", IMC)
+
+#Exercice 02 :
+def maxminmoy():
+    N = 0
+    Nombres = []
+    while N >= 0 :
+        N=int(input("Entrez un nombre  : "))
+        Nombres.append(N)
+    max = max(Nombres)
+    min = min(Nombres)
+    mous = sum(Nombres)/len(Nombres)
+    print("Le maximum est : ", max)
+    print("Le minimum est : ", min)
+    print("La moyenne est : ", mous)
+
+#Exercice 03 :
+def agecanineshumain():
+    age = int(input("Entrez l'âge du chien en années : "))
+    if age <= 2:
+        age_humain = age * 10.5
+    else:
+        age_humain = 21 + (age - 2) * 7
+    print("L'âge du chien en années humaines est : ", age_humain)
+
